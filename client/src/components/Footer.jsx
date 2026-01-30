59c823de302f2ef82087610a97926d9bcf354d82
AmenaTalukder <amenaakter9714@gmail.com>
2026-01-30 08:06:05 +0500

    frontend done

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -27,7 +27,7 @@ const Footer = () => {
                 </div>
 
                 <div>
-                    <p className='text-lg text-gray-800'>COMPANY</p>
+                    <p className=' font-playfair text-lg text-gray-800'>COMPANY</p>
                     <ul className='mt-3 flex flex-col gap-2 text-sm'>
                         <li><a href="#">About</a></li>
                         <li><a href="#">Careers</a></li>
@@ -38,7 +38,7 @@ const Footer = () => {
                 </div>
 
                 <div>
-                    <p className='text-lg text-gray-800'>SUPPORT</p>
+                    <p className='font-playfair text-lg text-gray-800'>SUPPORT</p>
                     <ul className='mt-3 flex flex-col gap-2 text-sm'>
                         <li><a href="#">Help Center</a></li>
                         <li><a href="#">Safety Information</a></li>
@@ -65,7 +65,7 @@ const Footer = () => {
             <hr className='border-gray-300 mt-8' />
             <div className='flex flex-col md:flex-row gap-2 items-center justify-between py-5'>
                 <p>© {new Date().getFullYear()} QuickStay. All rights reserved.</p>
-                <ul className='flex items-center gap-4'>
+                <ul className=' font-playfair flex items-center gap-4'>
                     <li><a href="#">Privacy</a></li>
                     <li><a href="#">Terms</a></li>
                     <li><a href="#">Sitemap</a></li>
